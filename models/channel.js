ca85d9e80d4f29411926f82d76d71b1bd16226cf
omar <omar.csse@gmail.com>
2019-07-01 10:27:59 +1000

    use async/await instead of promises

--- models/channel.js
+++ models/channel.js
@@ -6,22 +6,25 @@ let loadModel = async () => {
 }
 
 let io = (io) => {  
+
     io.on('connection', (socket) => { 
-        socket.on('color', (data) => {
-            predict(data.color[0], data.color[1], data.color[2]).then((i) => {
-                socket.emit('prediction', {prediction: colorsLabels[i]});
-            });
+
+        socket.on('color', async (data) => {
+            let i = await predict(data.color[0], data.color[1], data.color[2]);
+            socket.emit('prediction', {prediction: colorsLabels[i]});
         });
+
         socket.on('disconnect', () => {
             socket.conn.close();
         });
+
     });
+
 };
 
 const predict = async (r, g, b) => {
-    return loadModel().then((model) => {
-        return model.predict(tf.tensor2d([[r/255, g/255, b/255]])).argMax(1).dataSync()[0]
-    });
+    let model = await loadModel()
+    return await model.predict(tf.tensor2d([[r/255, g/255, b/255]])).argMax(1).dataSync()[0]
 }
 
 let self = module.exports = {
